43a3c5bc2315c8733e12e909274898edc670e3b6
Aluisio Lucio <aluisio18lucio@gmail.com>
2022-03-04 21:39:42 -0300

    Add render template html; Add script run-dev.sh para rodar localmente

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request, response_class=HTMLResponse):
+    return templates.TemplateResponse("index.html", {"request": request, "text": "Hello World!"})
